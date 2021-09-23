be79e38bad7b7956bfb51a77d658c301dc8bd03a
kamioka24 <keee2404@gmail.com>
2021-09-23 11:47:05 +0900

    [Finish] station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,18 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise(function(resolve) {
+    userList.map((data) => {
+      const fullName = data.family_name + " " + data.first_name
+      data["full_name"] = fullName
+    });
+    setTimeout(function() {
+      resolve(userList);
+    }, 3000);
+  });
 }
-
